e962f32e092b0bbea0018e8ec945adf301dc90c8
saikumarkale <saikumarkale1998@gmail.com>
2022-12-13 20:34:09 +0530

    Delete vowel or consonent.js

--- vowel or consonent.js
+++
@@ -1,6 +0,0 @@
-/**
- * take the input character from user
- * validate if its alphabet or not (a to z). else it is not a valid input.
- * if yes, then compare the input with a e i o u
- * if present in these letters , log it. else it is a consonent.
- */
