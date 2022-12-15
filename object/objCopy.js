f40407b19585f7b804818e5e2e5c471ff9d6d212
saikumarkale <saikumarkale1998@gmail.com>
2022-12-15 09:04:55 +0530

    ''

--- object/objCopy.js
+++ object/objCopy.js
@@ -1,3 +1,6 @@
+
+//with assign method....will be able to copy with out changing the previous obj value
+
 const person = { name: "sai", job: "se" };
 
 const details = Object.assign(person, { height: "5.10", age: "23" });
@@ -9,3 +12,17 @@ const fulldetails = Object.assign(person, details, { weight: "70kg", name: "saik
 
 
 console.log(fulldetails);
+
+
+
+//with spread method
+const prof1 = {
+    name: "kale"
+}
+const prof2 = {
+    middle: "sai", lastname: "kumar"
+}
+
+const fullname = { ...prof1, ...prof2 }
+
+console.log(fullname);
