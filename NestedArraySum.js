977e3c1d6769600ad704530a7e0ceaeec747ce99
saikumarkale <saikumarkale1998@gmail.com>
2023-01-30 21:50:57 +0530

    'nestedArraySum'

--- NestedArraySum.js
+++ NestedArraySum.js
@@ -1,21 +1,28 @@
-let input = [
+/**
+ * take the input from the user and itrate the array and null integer
+ * if the index[elem]is array then consider that as a seperate array and recurse untill the last nestede array.
+ * if its not arry then we can add the index values to the empty indeger
+ *
+ */
+
+let inp = [
     [1, 2, [3, [4], [5, 6]], [7]]
 ];
 let outPutArray = [];
 let sum = null;
-function sumOfNestedArrayElements(inputArray) {
+function sumOfNestedArrayElements(input) {
 
-    for (let i = 0; i < inputArray.length; i++) {
-        if (!Array.isArray(inputArray[i])) {
+    for (let i = 0; i < input.length; i++) {
+        if (!Array.isArray(input[i])) {
 
-            sum += inputArray[i];
+            sum += input[i];
         }
-        else if (Array.isArray(inputArray[i])) {
-            sumOfNestedArrayElements(inputArray[i]);
+        else if (Array.isArray(input[i])) {
+            sumOfNestedArrayElements(input[i]);
         }
 
     }
 
     return "sumOfNestedArrayElements :" + sum;
 }
-console.log(sumOfNestedArrayElements(input));
+console.log(sumOfNestedArrayElements(inp));
